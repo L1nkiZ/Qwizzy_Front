e9ae2d917f77000665bcb0d945fea8eb10b182df
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-11-04 16:21:18 +0100

    feat(connexion): add authentication form with email and password fields

---
+++ app/pages/connexion.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { AuthFormField } from "@nuxt/ui";
+
+const fields = ref<AuthFormField[]>([
+	{
+		name: "email",
+		type: "text",
+		label: "Email",
+	},
+	{
+		name: "password",
+		type: "password",
+		label: "Mot de passe",
+	},
+]);
+</script>
+
+<template>
+	<div class="flex flex-1 items-center justify-center">
+		<UPageCard class="w-full max-w-md">
+			<UAuthForm
+				title="Connexion"
+				description="Entrez vos identifiants pour accéder à votre compte."
+				icon="i-lucide-user"
+				:fields="fields"
+				class="max-w-md"
+			/>
+		</UPageCard>
+	</div>
+</template>
