c8ea278f15ef358815d233a460ef669e13094aea
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-11-24 11:42:25 +0100

    fix(eslint): update file naming conventions and rules for components, pages, and services

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -41,12 +41,56 @@ export default withNuxt(
 		},
 	},
 	{
-		files: ["app/components/**/*.vue", "app/pages/*/**/*.vue"],
+		files: ["app/components/**/*.vue"],
 		plugins: {
 			"check-file": checkFile,
 		},
 		rules: {
-			"check-file/no-index": "error",
+			"check-file/filename-blocklist": [
+				"error",
+				{
+					"**/index.vue": "*.vue",
+					"**/Index.vue": "*.vue",
+					"**/INDEX.vue": "*.vue",
+				},
+			],
+			"check-file/folder-naming-convention": [
+				"error",
+				{
+					"app/components/**/*": "PASCAL_CASE",
+				},
+			],
+		},
+	},
+	{
+		files: ["app/pages/**/*.*"],
+		plugins: {
+			"check-file": checkFile,
+		},
+		rules: {
+			"check-file/folder-naming-convention": [
+				"error",
+				// tout sauf les dossiers spéciaux entre crochets comme [id] ou entre parenthèses comme (admin)
+				{
+					"app/pages/**/!(\\[*\\]|\\(*\\))/": "KEBAB_CASE",
+				},
+			],
+		},
+	},
+	{
+		files: ["app/services/**/*.*", "app/stores/**/*.*", "app/types/**/*.*"],
+		plugins: {
+			"check-file": checkFile,
+		},
+		rules: {
+			"check-file/filename-naming-convention": [
+				"error",
+				{
+					"app/services/**/*.!(test|spec).*": "*.service",
+					"app/stores/**/*.!(test|spec).*": "*.store",
+					"app/types/**/*.!(test|spec).*": "*.type",
+				},
+			],
 		},
 	},
 	eslintConfigPrettier,
