cf84f07d7d5ca2ab3d5e465f61f10a0cbda54a9b
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-11-24 11:47:26 +0100

    feat(layout): add admin layout with header, footer, and navigation menus

---
+++ app/layouts/admin.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from "@nuxt/ui";
+
+const route = useRoute();
+
+const headerItems = computed<NavigationMenuItem[]>(() => [
+	{
+		label: "Qwizz",
+		to: "/admin/qwizz/",
+		active: route.path.startsWith("/admin/qwizz/"),
+	},
+	{
+		label: "Catégories",
+		to: "/admin/categories/",
+		active: route.path.startsWith("/admin/categories/"),
+	},
+	{
+		label: "Utilisateurs",
+		to: "/admin/users/",
+		active: route.path.startsWith("/admin/users/"),
+	},
+]);
+
+const footerItems: NavigationMenuItem[] = [
+	{
+		label: "Mentions légales",
+		to: "/mentions-legales",
+	},
+	{
+		label: "Politique de confidentialité",
+		to: "/politique-de-confidentialite",
+	},
+	{
+		label: "Contact",
+		to: "/contact",
+	},
+];
+</script>
+
+<template>
+	<UHeader>
+		<template #title>Qwizzy - Admin</template>
+		<UNavigationMenu :items="headerItems" />
+		<template #right>
+			<UButton
+				icon="i-lucide-user"
+				color="neutral"
+				variant="ghost"
+				to="/compte"
+			></UButton>
+			<UColorModeButton />
+		</template>
+
+		<template #body>
+			<UNavigationMenu :items="headerItems" orientation="vertical" />
+		</template>
+	</UHeader>
+
+	<main
+		class="bg-muted min-h-[calc(100dvh-var(--ui-header-height))] px-4 py-8 md:px-6 md:py-10 lg:px-8 lg:py-12"
+	>
+		<slot></slot>
+	</main>
+
+	<!-- eslint-disable-next-line -->
+	<UFooter>
+		<template #left>
+			<p class="text-muted text-sm">
+				Copyright © {{ new Date().getFullYear() }}
+			</p>
+		</template>
+		<UNavigationMenu
+			:items="footerItems"
+			variant="link"
+			:ui="{
+				list: 'flex-col sm:flex-row',
+			}"
+		/>
+		<template #right>
+			<UButton
+				icon="i-simple-icons-github"
+				color="neutral"
+				variant="ghost"
+				to="https://github.com/L1nkiZ/Qwizzy_Front"
+				target="_blank"
+				aria-label="GitHub"
+			/>
+		</template>
+	</UFooter>
+</template>
