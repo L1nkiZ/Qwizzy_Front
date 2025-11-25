9942b0203138fe6e4696ca977d4c9d5399eab3cf
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-11-25 14:47:24 +0100

    feat(apiFetch): add docker configuration to determine base URL for API requests

--- app/plugins/apiFetch.ts
+++ app/plugins/apiFetch.ts
@@ -1,11 +1,11 @@
 export default defineNuxtPlugin(() => {
 	// const { session } = useUserSession();
 	const {
-		public: { baseApiURL, nginx },
+		public: { baseApiURL, nginx, docker },
 	} = useRuntimeConfig();
 
 	// Determine the base URL for API requests (check if running on server or client + if there is nginx proxy)
-	const baseUrl: string = import.meta.server
+	const baseUrl: string = docker
 		? "http://api:8000/api/"
 		: nginx
 			? "/api/"
--- app/services/admin/questions.service.ts
+++ app/services/admin/questions.service.ts
@@ -1,11 +1,9 @@
-import type { Question } from "~/types/question.type";
-
 /**
  * Récupère la liste de toutes les questions
  * @returns Promise contenant les données des questions et une éventuelle erreur
  */
 export function fetchQuestions() {
-	return useApiFetch<Question[]>("/questions", {
+	return useApiFetch("/questions", {
 		params: {
 			current_sort: "id",
 			current_sort_dir: "asc",
