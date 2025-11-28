65ee28f57a3bf60faebb140d02ec36ca91c93f89
Nicolas Walter <nicolaswalter2003@gmail.com>
2025-11-28 12:38:28 +0100

    feat(questions): implement fetching of answers and enhance question form with subjects and additional proposals

--- app/components/Admin/Questions/Modal/AddOrEdit.vue
+++ app/components/Admin/Questions/Modal/AddOrEdit.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import * as v from "valibot";
+import { fetchAnswer } from "~/services/admin/answer.service";
+import { fetchSubjects } from "~/services/admin/subjects.service";
 import type { Question } from "~/types/question.type";
 
 interface Props {
@@ -7,27 +9,70 @@ interface Props {
 }
 const props = defineProps<Props>();
 
-const isEditing = computed(() => !!props.question);
+const editionMode = computed(() => !!props.question);
+
+const { data: subjects, status: subjectsStatus } = await fetchSubjects();
+
+const subjectsItems = computed(() =>
+	subjects.value
+		? (
+				subjects.value as { subject: { data: { id: number; name: string }[] } }
+			).subject.data.map((subject) => ({
+				value: subject.id,
+				label: subject.name,
+			}))
+		: [],
+);
+
+const answer = ref<any>(null);
+
+if (editionMode.value) {
+	const { data: answerData } = await fetchAnswer(props.question?.id ?? 0);
+	answer.value = answerData;
+}
+
+const questionAnswer = computed(() =>
+	answer.value
+		? (
+				answer.value as {
+					question: {
+						answers: {
+							id: number;
+						};
+					};
+				}
+			).question.answers.id
+		: null,
+);
 
 const form = useTemplateRef<HTMLFormElement>("form");
 const formId = useId();
 
 const state = reactive<{
 	title: string;
-	subject_id: number;
+	subjects: { value: number; label: string }[];
 	difficulty_id: number;
 	proposal_1: string;
 	proposal_2: string;
 	proposal_3: string;
-	answer: string;
+	proposal_4: string;
+	answer: number;
 }>({
-	title: "",
-	subject_id: 0,
-	difficulty_id: 0,
-	proposal_1: "",
-	proposal_2: "",
-	proposal_3: "",
-	answer: "",
+	title: props.question?.question || "",
+	subjects: props.question?.subject
+		? [
+				{
+					value: props.question.subject_id,
+					label: props.question.subject,
+				},
+			]
+		: [],
+	difficulty_id: props.question?.difficulty.id || 0,
+	proposal_1: props.question?.proposal_1 || "",
+	proposal_2: props.question?.proposal_2 || "",
+	proposal_3: props.question?.proposal_3 || "",
+	proposal_4: props.question?.proposal_4 || "",
+	answer: questionAnswer?.value || 1,
 });
 
 const schema = v.object({
@@ -35,7 +80,12 @@ const schema = v.object({
 		v.string(),
 		v.minLength(10, "Veuillez entrer un titre de minimum 10 caractères."),
 	),
-	subject_id: v.number(),
+	subjects: v.array(
+		v.object({
+			value: v.number(),
+			label: v.string(),
+		}),
+	),
 	difficulty_id: v.number(),
 	proposal_1: v.string(),
 	proposal_2: v.string(),
@@ -59,7 +109,7 @@ async function updateOrCreate() {
 
 <template>
 	<UModal
-		:title="isEditing ? 'Modifier la question' : 'Ajouter une question'"
+		:title="editionMode ? 'Modifier la question' : 'Ajouter une question'"
 		:description="props.question?.question"
 		:ui="{ footer: 'justify-end' }"
 		class="max-w-120"
@@ -81,9 +131,17 @@ async function updateOrCreate() {
 						minlength="10"
 					/>
 				</UFormField>
-				<UFormField required name="subject_id" label="Sujet" class="mb-6">
-					<UInputMenu v-model="state.subject_id" multiple :items="subjects" />
+				<UFormField required name="subject_id" label="Sujet">
+					<UInputMenu
+						v-model="state.subjects"
+						multiple
+						:loading="subjectsStatus === 'idle'"
+						:items="subjectsItems"
+					/>
 				</UFormField>
+
+				<USeparator label="Réponses" class="mt-8 mb-7" />
+
 				<UFormField required name="proposal_1" label="Proposition 1">
 					<UInput v-model="state.proposal_1" />
 				</UFormField>
@@ -93,8 +151,12 @@ async function updateOrCreate() {
 				<UFormField required name="proposal_3" label="Proposition 3">
 					<UInput v-model="state.proposal_3" />
 				</UFormField>
+				<UFormField required name="proposal_4" label="Proposition 4">
+					<UInput v-model="state.proposal_4" />
+				</UFormField>
+
 				<UFormField required name="answer" label="Réponse correcte">
-					<UInput v-model="state.answer" />
+					<UInput v-model="state.answer" type="number" min="1" max="4" />
 				</UFormField>
 			</UForm>
 		</template>
