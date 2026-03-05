aa61827ff13c926189ebfa974e3380e71ada3238
Nicolas Walter <nicolaswalter2003@gmail.com>
2026-03-05 13:57:04 +0100

    feat(login): refactor the pages with a component

--- app/pages/admin/connexion.vue
+++ app/pages/admin/connexion.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import * as v from "valibot";
-
-import { login } from "~/services/admin/auth.service";
-
-import type { AuthFormField, FormSubmitEvent } from "@nuxt/ui";
-
 useHead({
 	title: "Qwizzy - Connexion",
 	meta: [
@@ -14,76 +8,12 @@ useHead({
 		},
 	],
 });
-
-const fields = ref<AuthFormField[]>([
-	{
-		required: true,
-		name: "email",
-		type: "email",
-		label: "Email",
-	},
-	{
-		required: true,
-		name: "password",
-		type: "password",
-		label: "Mot de passe",
-	},
-	{
-		required: true,
-		name: "remember",
-		label: "Remember me",
-		type: "checkbox",
-		defaultValue: true,
-	},
-]);
-
-const schema = v.object({
-	email: v.pipe(
-		v.string("Veuillez entrer une adresse email."),
-		v.email("Veuillez entrer une adresse email valide."),
-	),
-	password: v.pipe(v.string("Le mot de passe doit être renseigné.")),
-	remember: v.boolean("Veuillez indiquer si vous souhaitez rester connecté."),
-});
-
-type Schema = v.InferOutput<typeof schema>;
-
-async function onSubmit(payload: FormSubmitEvent<Schema>) {
-	await login(payload.data.email, payload.data.password)
-		.then((response) => {
-			const adminTokenCookie = useCookie(
-				"adminTokenCookie",
-				payload.data.remember
-					? {
-							maxAge: 60 * 60 * 24 * 30,
-						}
-					: undefined,
-			); // Cookie valable pendant 30 jours si "Remember me" est coché, sinon cookie de session
-
-			adminTokenCookie.value = response.token;
-
-			navigateTo("/admin");
-		})
-		.catch((error) => {
-			useErrorToast({
-				title: "Erreur lors de la connexion",
-				description: error.message,
-			});
-		});
-}
 </script>
 
 <template>
-	<div class="mt-20 flex flex-1 items-center justify-center p-4">
-		<UPageCard class="w-full max-w-md">
-			<UAuthForm
-				title="Connexion"
-				icon="i-lucide-user"
-				:fields
-				:schema
-				class="max-w-md"
-				@submit="onSubmit"
-			/>
-		</UPageCard>
-	</div>
+	<AuthLoginForm
+		cookie-name="adminTokenCookie"
+		redirect-to="/admin"
+		title="Connexion"
+	/>
 </template>
--- app/pages/connexion.vue
+++ app/pages/connexion.vue
@@ -1,10 +1,4 @@
 <script setup lang="ts">
-import * as v from "valibot";
-
-import { login } from "~/services/admin/auth.service";
-
-import type { AuthFormField, FormSubmitEvent } from "@nuxt/ui";
-
 useHead({
 	title: "Qwizzy - Connexion",
 	meta: [
@@ -14,81 +8,15 @@ useHead({
 		},
 	],
 });
-
-const fields = ref<AuthFormField[]>([
-	{
-		required: true,
-		name: "email",
-		type: "email",
-		label: "Email",
-	},
-	{
-		required: true,
-		name: "password",
-		type: "password",
-		label: "Mot de passe",
-	},
-	{
-		required: true,
-		name: "remember",
-		label: "Remember me",
-		type: "checkbox",
-		defaultValue: true,
-	},
-]);
-
-const schema = v.object({
-	email: v.pipe(
-		v.string("Veuillez entrer une adresse email."),
-		v.email("Veuillez entrer une adresse email valide."),
-	),
-	password: v.pipe(v.string("Le mot de passe doit être renseigné.")),
-	remember: v.boolean("Veuillez indiquer si vous souhaitez rester connecté."),
-});
-
-type Schema = v.InferOutput<typeof schema>;
-
-async function onSubmit(payload: FormSubmitEvent<Schema>) {
-	await login(payload.data.email, payload.data.password)
-		.then((response) => {
-			const userTokenCookie = useCookie(
-				"userTokenCookie",
-				payload.data.remember
-					? {
-							maxAge: 60 * 60 * 24 * 30,
-						}
-					: undefined,
-			); // Cookie valable pendant 30 jours si "Remember me" est coché, sinon cookie de session
-
-			userTokenCookie.value = response.token;
-
-			navigateTo("/");
-		})
-		.catch((error) => {
-			// Gérer les erreurs de connexion, par exemple en affichant un message d'erreur
-			console.error("Erreur de connexion :", error);
-		});
-}
 </script>
 
 <template>
-	<div class="mt-20 flex flex-1 items-center justify-center p-4">
-		<UPageCard class="w-full max-w-md">
-			<UAuthForm
-				title="Connexion"
-				icon="i-lucide-user"
-				:fields
-				:schema
-				class="max-w-md"
-				@submit="onSubmit"
-			>
-				<template #description>
-					Vous n'avez pas de compte ?
-					<ULink to="/inscription" class="text-primary font-medium">
-						Inscrivez-vous
-					</ULink>
-				</template>
-			</UAuthForm>
-		</UPageCard>
-	</div>
+	<AuthLoginForm title="Connexion">
+		<template #description>
+			Vous n'avez pas de compte ?
+			<ULink to="/inscription" class="text-primary font-medium">
+				Inscrivez-vous
+			</ULink>
+		</template>
+	</AuthLoginForm>
 </template>
