292b51544d725a8ee912afa02db4df25bb71d024
Nicolas Walter <nicolaswalter2003@gmail.com>
2026-03-05 14:49:40 +0100

    feat(admin dahsboard): add indicator card

--- app/pages/admin/index.vue
+++ app/pages/admin/index.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { fetchCategories } from "~/services/admin/categories.service";
+import { fetchDifficulties } from "~/services/admin/difficulties.service";
+import { fetchQuestions } from "~/services/admin/questions.service";
+
+const { data: questions } = await fetchQuestions();
+const { data: categories } = await fetchCategories();
+const { data: difficulties } = await fetchDifficulties();
+</script>
 
 <template>
 	<div>
-		<h1>Bienvenue sur l'administration de Qwizzy!</h1>
+		<h1>Bienvenue sur l'administration de Qwizzy !</h1>
+
+		<UPageGrid>
+			<UPageCard title="Nombre de questions créées :" spotlight>
+				<span class="text-4xl">{{ questions?.questions.data.length }}</span>
+			</UPageCard>
+			<UPageCard title="Nombre de catégories créées :" spotlight>
+				<span class="text-4xl">{{ categories?.subject.data.length }}</span>
+			</UPageCard>
+			<UPageCard title="Nombre de difficultés créées :" spotlight>
+				<span class="text-4xl">{{ difficulties?.difficulty.data.length }}</span>
+			</UPageCard>
+		</UPageGrid>
 	</div>
 </template>
